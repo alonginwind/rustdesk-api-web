c583c42e3269b3be7cbdd3b3eedab28db44e02a1
alonginwind <sunzl3721@gmail.com>
2026-05-07 16:17:44 +0800

    修复access_token一致性

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -7,7 +7,6 @@ export function getToken () {
 }
 
 export function setToken (token) {
-  localStorage.setItem(`wc-option:local:access_token`, token)
   return localStorage.setItem(TokenKey, token)
 }
 
